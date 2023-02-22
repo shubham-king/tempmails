c3c95907c40441c4ade511882893f175b5e14a66
shubham-king <84066395+shubham-king@users.noreply.github.com>
2023-02-22 13:55:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def help_command(update: Update, _: CallbackContext) -> None:
 
 def main() -> None:
     # Create the Updater and pass it your bot's token.
-    updater = Updater("1958178561:AAHJ8X6TpBzjn-v4tMtOFhFfMZHfSxXavfk") #API Token here
+    updater = Updater("6221156092:AAEqxcPTxyv-OGfU2umfQ2GpLHmKsMwq31o") #API Token here
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
     #updater.dispatcher.add_handler(CallbackQueryHandler(button))
